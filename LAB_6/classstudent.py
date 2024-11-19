025bc06306d63c39ac5a2dfbf67419c6c4dd6191
Anmol-Prabhakar <anmol99prabhakar@gmail.com>
2024-11-19 18:36:33 +0530

    Add files via upload

---
+++ LAB_6/classstudent.py
@@ -0,0 +1,8 @@
+class student:
+    def __init__(self,name,rollno):
+        self.name = name
+        self.rollno = rollno
+
+student1 = student("John",2)
+
+print(f"student 1 details: name - {student1.name} , rollno - {student1.rollno}")
